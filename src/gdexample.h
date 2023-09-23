40055841720f9cdf650acc31908d7f80f235af61
PierreEVEN <evenpierre@orange.fr>
2023-09-23 17:38:46 +0200

    added procedural terrain class, but there is an issue with bind_method

--- src/gdexample.h
+++ src/gdexample.h
@@ -2,20 +2,22 @@
 
 #include <godot_cpp/classes/sprite2d.hpp>
 
-namespace godot {
-class GDExample : public Sprite2D {
-    GDCLASS(GDExample, Sprite2D)
+namespace godot
+{
+	class GDExample : public Sprite2D
+	{
+		GDCLASS(GDExample, Sprite2D)
 
-private:
-    double time_passed;
+	private:
+		double time_passed;
 
-protected:
-    static void _bind_methods();
+	protected:
+		static void _bind_methods();
 
-public:
-    GDExample();
-    ~GDExample();
+	public:
+		GDExample();
+		~GDExample();
 
-    void _process(double delta);
-};
+		void _process(double delta);
+	};
 }
